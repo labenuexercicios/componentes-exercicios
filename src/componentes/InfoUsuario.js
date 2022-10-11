36740f2e9d602ee8e1534e914d0756e0ad10ed1c
GLCalegaro <giovaluiza@gmail.com>
2022-10-11 16:12:00 -0300

    Arquivos corretos

---
+++ src/componentes/InfoUsuario.js
@@ -0,0 +1,10 @@
+import React from "react";
+function InfoUsuario(){
+    return (
+    <>
+    <img src="https://picsum.photos/400/400?a=5 " alt="" />
+    <p>Nome do usuário: Usuário 1</p>
+    </>
+    )
+}
+export default InfoUsuario;
