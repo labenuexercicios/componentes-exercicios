7fbf6ffdec8f3d0a9349f3ef5a6ac9341941050a
Arthuramaral100 <arthuramaral57@gmail.com>
2022-10-11 15:38:38 -0300

    Exercicios a031-componentes

---
+++ src/components/CardVideo.js
@@ -0,0 +1,18 @@
+import React from "react"
+import {InfosUsuario} from "./InfosUsuario"
+
+export function CardVideo(){
+    const titulo = "Título do vídeo";
+    function reproduzVideo() {
+        alert("O vídeo está sendo reproduzido");}
+        
+    return(
+        <div>
+            <div className="box-pagina-principal" onClick={reproduzVideo}>
+              <img src="https://picsum.photos/400/400?a=1 " alt="" />
+              <h4>{titulo}</h4>
+              <InfosUsuario/>
+            </div>
+        </div>
+    )
+}
