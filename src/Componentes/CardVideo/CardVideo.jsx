d5293cdf08d9c8c242345e3a48a64f328ff9c819
Lu4n-M0ur4 <luan.moura-rj@hotmail.com>
2023-05-12 18:45:42 -0300

    exercicio componentes labetube

---
+++ src/Componentes/CardVideo/CardVideo.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import InfosUsuario from "../InfosUsuario/InfosUsuario";
+import "./styles.css"
+
+const CardVideo = () => {
+    const titulo = "Título do vídeo";
+    
+    function reproduzVideo() {
+      alert("O vídeo está sendo reproduzido");
+    }
+ 
+      return (
+    <div className="box-pagina-principal" onClick={reproduzVideo}>
+    <img src="https://picsum.photos/400/400?a=1 " alt="" />
+    <h4>{titulo}</h4>
+   <InfosUsuario/>
+  
+  </div>
+  )
+}
+
+
+
+export default CardVideo;
---
+++ src/Componentes/InfosUsuario/InfosUsuario.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css"
+
+const InfosUsuario = () => {
+  return <div>
+<img src="" alt="" />
+<h1>Mario</h1>
+  </div>;
+};
+
+export default InfosUsuario;
